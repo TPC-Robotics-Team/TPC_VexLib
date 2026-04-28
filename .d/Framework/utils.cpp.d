25cdffaa8ba9a427dae47bcd478e189ad4832f41
AvidDavid23579 <aviddavid23579@gmail.com>
2026-04-28 21:54:52 +0700

    Added training folder

---
+++ .d/Framework/utils.cpp.d
@@ -0,0 +1,13 @@
+bin/Framework/utils.cpp.o: src/Framework/utils.cpp include/utils.hpp \
+ include/pros/imu.hpp include/pros/device.hpp include/pros/misc.hpp \
+ include/pros/misc.h include/pros/rtos.hpp include/pros/rtos.h \
+ include/pros/imu.h include/pros/imu.hpp
+include/utils.hpp:
+include/pros/imu.hpp:
+include/pros/device.hpp:
+include/pros/misc.hpp:
+include/pros/misc.h:
+include/pros/rtos.hpp:
+include/pros/rtos.h:
+include/pros/imu.h:
+include/pros/imu.hpp:
---
+++ training_files/training_README.md
@@ -0,0 +1,3 @@
+Comment cẩn thận đầy đủ, đảm bảo đầy đủ chức năng TeleOp và có Autonomous cơ bản.
+
+Sáng tạo lên nhé ae, t hóng.
