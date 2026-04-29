10e515936fb32fdf04235b54135f639e0536c393
AvidDavid23579 <aviddavid23579@gmail.com>
2026-04-29 18:48:51 +0700

    Added header files for odometry for later implementation

--- include/controller.hpp
+++ include/controller.hpp
@@ -2,6 +2,9 @@
 #include "pros/imu.hpp"
 #include "pros/motor_group.hpp"
 #include "cmath"
+#include "pros/rotation.hpp"
+
+constexpr double DEG_TO_RAD = M_PI / 180;
 
 
 // Slew Delimitation
@@ -83,26 +86,6 @@ struct Pose{
 };
 
 
-class EncoderOdometry{
-private:
-    pros::MotorGroup& m_leftMotorGroup;
-    pros::MotorGroup& m_rightMotorGroup;
-    double m_wheelCirc; // cm
-    double m_gearRatio;
-    double m_lastPosition = 0.0;
-    
-    double degreesToCm(double degrees);
-
-public:
-    EncoderOdometry(pros::MotorGroup& leftMotors, pros::MotorGroup& rightMotors, double wheelDiam, double gearRatio = 1.0);
-
-    double getPosition();
-    double getDelta();
-    double getVelocity();
-    double getPose();
-    void reset(); 
-};
-
 
 struct ProfileConstraints{
     double maxVelocity;
@@ -138,3 +121,67 @@ public:
     double totalTime();
 };
 
+
+// Odometry
+class EncoderOdometry{
+private:
+    pros::MotorGroup& m_leftMotorGroup;
+    pros::MotorGroup& m_rightMotorGroup;
+    double m_wheelCirc; // cm
+    double m_gearRatio;
+    double m_lastLeft = 0.0;
+    double m_lastRight = 0.0;
+    double m_last_time = 0.0;
+    
+    double degreesToCm(double degrees);
+
+public:
+    EncoderOdometry(pros::MotorGroup& leftMotors, pros::MotorGroup& rightMotors, double wheelDiam, double gearRatio = 1.0);
+
+    double getPosition();
+    double getDelta();
+    double getVelocity();
+    Pose getPose();
+    void reset(); 
+};
+
+class TwoWheelOdometry{
+private:
+    pros::Rotation& m_parallel;
+    pros::Rotation& m_perpendicular;
+    pros::Imu& m_imu;
+
+    double m_wheel_circ;
+
+    double m_parallel_offset;
+    double m_perpendicular_offset;
+
+    double m_x = 0.0;
+    double m_y = 0.0;
+    double m_theta = 0.0;
+
+    double m_last_parallel = 0.0;
+    double m_last_perpendicular = 0.0;
+    double m_last_theta = 0.0;
+
+    double m_last_time = 0.0;
+
+    bool m_initialized = false;
+public:
+    TwoWheelOdometry(
+        pros::Rotation& parallel,
+        pros::Rotation& perpendicular,
+        pros::Imu& imu,
+        double wheel_Diam,
+        double parallel_offset,
+        double perpendicular_offset
+    );
+
+    void update();
+    void reset();
+    double getX();
+    double getY();
+    double getTheta();
+    Pose getPose();
+    double getVelocity();
+};
