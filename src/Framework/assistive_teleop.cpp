2ce983991d0d1c600d7503d6b3053d67eb78fc8d
AvidDavid23579 <aviddavid23579@gmail.com>
2026-05-02 21:31:22 +0700

    Re-added Heading Hold

--- src/Framework/assistive_teleop.cpp
+++ src/Framework/assistive_teleop.cpp
@@ -1,5 +1,6 @@
 #include "custom/assistive_teleop.hpp"
 #include "custom/const.hpp"
+#include "custom/utils.hpp"
 
 #include <cmath>
 
@@ -26,4 +27,46 @@ double shapeInput(double input, double a)
     double x = input / MAX_JOYSTICK;
     double shaped = a * x * x * x + (1 - a) * x;
     return shaped * 127;
+}
+
+// Heading Hold
+HeadingHold::HeadingHold(pros::Imu& imu, PID pid) : imu(imu), controller(pid), targetHeading(0), enabled(false) {}
+
+double HeadingHold::update(double throttleInput, double steerInput)
+{
+
+    const double deadband = 10;
+
+    double heading = imu.get_rotation();
+
+    if ((std::abs(steerInput) > deadband))
+    {
+        controller.reset();
+        return steerInput;
+    }
+
+    if (!enabled)
+    {
+        targetHeading = heading;
+        controller.reset();
+        enabled = true;
+    }
+
+    double error = wrapAngle(targetHeading - heading);
+
+    double correction = controller.update(0, -error);
+
+    return correction;
+}
+
+void HeadingHold::setTarget(double target)
+{
+    targetHeading = target;
+}
+
+void HeadingHold::reset()
+{
+    targetHeading = imu.get_rotation();
+    controller.reset();
+    enabled = false;
 }
--- test_files/Control Tuning/FeedForwardTune.cpp
+++ test_files/Control Tuning/FeedForwardTune.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 #include "pros/misc.h"
-#include "auton.hpp"
+#include "custom/auton.hpp"
 
 
 
